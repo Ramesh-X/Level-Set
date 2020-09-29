1644c0c3cb808dd700ac2bc37c1b38a9f91b88a7
ramesh <rsoft.ramesh@gmail.com>
2020-09-29 09:56:04 +0530

    change copyright info

--- lv_set/show_fig.py
+++ lv_set/show_fig.py
@@ -1,3 +1,16 @@
+"""
+This python code demonstrates an edge-based active contour model as an application of the
+Distance Regularized Level Set Evolution (DRLSE) formulation in the following paper:
+
+  C. Li, C. Xu, C. Gui, M. D. Fox, "Distance Regularized Level Set Evolution and Its Application to Image Segmentation",
+     IEEE Trans. Image Processing, vol. 19 (12), pp. 3243-3254, 2010.
+
+Author: Ramesh Pramuditha Rathnayake
+E-mail: rsoft.ramesh@gmail.com
+
+Released Under MIT License
+"""
+
 import matplotlib.pyplot as plt
 import numpy as np
 from skimage import measure
